6f1074bee655d5f5eaa1b8c180ca18d67b5cd0db
BenYangMingLi <yml623930844@163.com>
2018-04-03 17:14:21 +0800

    create repository

---
+++ src/main/java/com/forezp/Receiver.java
@@ -0,0 +1,21 @@
+package com.forezp;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Component
+public class Receiver {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+        System.out.print("Received <" + message + ">");
+        latch.countDown();
+    }
+
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
